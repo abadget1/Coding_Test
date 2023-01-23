4878fba4a7211a09907645eefd0298a50d425437
Aaron Badgett <aaron.badgett@outlook.com>
2023-01-23 03:44:36 -0500

    Added test cases

---
+++ src/main/java/MagMutual/TakeHomeProject/UserAPI/model/User.java
@@ -0,0 +1,116 @@
+package MagMutual.TakeHomeProject.UserAPI.model;
+import java.time.LocalDate;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+
+@Entity
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotBlank
+    private String firstName;
+
+    @NotBlank
+    private String lastName;
+
+    @NotBlank
+    @Email
+    private String email;
+
+    @NotBlank
+    private String profession;
+
+    @NotNull
+    private LocalDate dateCreated;
+
+    @NotBlank
+    private String country;
+
+    @NotBlank
+    private String city;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getProfession() {
+        return profession;
+    }
+
+    public void setProfession(String profession) {
+        this.profession = profession;
+    }
+
+    public LocalDate getDateCreated() {
+        return dateCreated;
+    }
+
+    public void setDateCreated(LocalDate dateCreated) {
+        this.dateCreated = dateCreated;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+
+
+    public User(Long id, String firstName, String lastName, String email, String profession, LocalDate dateCreated, String country, String city){
+        this.id = id;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.email = email;
+        this.profession = profession;
+        this.dateCreated = dateCreated;
+        this.country = country;
+        this.city = city;
+    }
+    User(){}
+}
---
+++ src/test/java/MagMutual/TakeHomeProject/UserAPI/UserServiceTest.java
@@ -0,0 +1,96 @@
+package MagMutual.TakeHomeProject.UserAPI;
+import MagMutual.TakeHomeProject.UserAPI.model.User;
+import MagMutual.TakeHomeProject.UserAPI.services.UserService;
+
+import java.time.LocalDate;
+import java.util.List;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class UserServiceTest {
+
+    @Autowired
+    private UserService userService;
+
+    @BeforeEach
+    public void setUp() {
+        userService = new UserService();
+    }
+
+    @Test
+    public void testGetUsersByProfession_validProfession() {
+        List<User> users = userService.getUsersByProfession("worker");
+        assertNotNull(users);
+        assertTrue(users.size() > 0);
+        for (User user : users) {
+            assertEquals("worker", user.getProfession());
+        }
+    }
+
+    @Test
+    public void testGetUsersByProfession_invalidProfession() {
+        List<User> users = userService.getUsersByProfession("NotValid");
+        assertEquals(0, users.size());
+    }
+
+    @Test
+    public void testGetUsersByDateRange_validRange() {
+        LocalDate startDate = LocalDate.of(2020, 1, 1);
+        LocalDate endDate = LocalDate.of(2020, 12, 31);
+        List<User> users = userService.getUsersByDateRange(startDate, endDate);
+        assertNotNull(users);
+        assertTrue(users.size() > 0);
+        for (User user : users) {
+            assertTrue(user.getDateCreated().isAfter(startDate) && user.getDateCreated().isBefore(endDate));
+        }
+    }
+
+    @Test
+    public void testGetUsersByDateRange_invalidRange() {
+        LocalDate startDate = LocalDate.of(2022, 1, 1);
+        LocalDate endDate = LocalDate.of(2020, 12, 31);
+        List<User> users = userService.getUsersByDateRange(startDate, endDate);
+        assertNotNull(users);
+        assertEquals(0, users.size());
+    }
+
+    @Test
+    public void testGetUserByName_validName(){
+        User user = userService.getUserByName("Andree", "Flita");
+        assertNotNull(user);
+        assertTrue(user.getFirstName().equalsIgnoreCase("Andree") && user.getLastName().equalsIgnoreCase("Flita"));
+    }
+
+    @Test
+    public void testGetUserByName_invalidName(){
+        User user = userService.getUserByName("NotValid", "NotValid");
+        assertNull(user);
+    }
+
+    @Test
+    public void testGetUserById_validId(){
+        User user = userService.getUserById(100L);
+        assertEquals(100L, user.getId());
+    }
+
+    @Test
+    public void testGetUserById_invalidId(){
+        User user = userService.getUserById(1000000000000L);
+        assertNull(user);
+    }
+    @Test
+    public void testGetUserById_nullId() {
+        User user = userService.getUserById(null);
+        assertNull(user);
+    }
+    @Test
+    public void testGetUserAllUsers() {
+        List<User> users = userService.getAllUsers();
+        assertTrue(users.size() > 0);
+    }
+
+}
